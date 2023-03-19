b5f79733fb0f62e124fb2da653190c38835bf06e
TingSyuanWang <vincent@fishboneapps.com>
2023-03-18 23:31:32 -0400

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Ting-Hsuan Wang on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
